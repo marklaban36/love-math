a2681826d316c7648f1efa010fa159f53a8d3975
marklaban36 <marklaban32@gmail.com>
2025-09-05 14:24:54 +0100

    Add set up structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnwser() {
+
+}
+
+function calculateCorrectAnwser() {
+
+}
+
+function incermentScore() {
+
+}
+
+function incrementWrongAnwser() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
