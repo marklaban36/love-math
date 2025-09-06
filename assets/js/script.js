d0dec9cf596b553dad9d3b3e2de1622e1092e8bc
marklaban36 <marklaban32@gmail.com>
2025-09-06 11:39:16 +0100

    Display additon question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-//Wait for the DOM to finish loading before running the game
-//Get the button elements and add event listeners to them
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
@@ -10,41 +10,56 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
+
 });
 
 /**
- * The main game "loop", called when when the sctipt is first loaded
- * and after the user's anwser has been processed
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
  */
-function runGame() {
-    // Creates two random numbers between 1 and 25 
+function runGame(gameType) {
+
+    // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
-}
-
-function checkAnwser() {
-
-}
-
-function calculateCorrectAnwser() {
-
-}
-
-function incermentScore() {
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
-function incrementWrongAnwser() {
+function checkAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function calculateCorrectAnswer() {
 
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 }
 
 function displaySubtractQuestion() {
@@ -52,5 +67,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-
+    
 }
